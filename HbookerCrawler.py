bebc5e0819776288d8f788bdb13e053ca5aac480
SeaLoong <984391132@qq.com>
2017-08-25 07:18:25 +0800

    update

--- HbookerCrawler.py
+++ HbookerCrawler.py
@@ -56,7 +56,9 @@ os.environ["NODE_PATH"] = os.getcwd() + "/node_modules"
 Decrypt_js = execjs.compile(open(os.getcwd() + '/Decrypt.js').read())
 
 
-def decrypt(_chapter_content, _encryt_keys, _chapter_access_key):
+def decrypt(_chapter_content: str, _encryt_keys: str, _chapter_access_key: str):
+    _encryt_keys = _encryt_keys.replace('"', '').replace('\\', '')
+    _chapter_content = _chapter_content.replace('\\', '')
     ret = str(Decrypt_js.call('decrypt', _chapter_content, _encryt_keys, _chapter_access_key))
     ret = ret.replace("<p class='chapter'>", '')
     ret = ret.replace('</p>', '')
@@ -65,18 +67,33 @@ def decrypt(_chapter_content, _encryt_keys, _chapter_access_key):
 print("请先登录你的欢乐书客帐号，之后得到一些Cookies并输入程序。")
 print("若不登录则直接留空所有Cookies。")
 
-login_token = input("Cookie: login_token=")
-reader_id = input("Cookie: reader_id=")
-user_id = input("Cookie: user_id=(为空则与 reader_id 相同)") or reader_id
-# login_token = ""
-# reader_id = ""
-# user_id = ""
+login_token = ""
+reader_id = ""
+if not os.path.isdir(os.getcwd() + "\\..\\books"):
+    os.mkdir(os.getcwd() + "\\..\\books")
+if os.path.isfile(os.getcwd() + "\\..\\books\\hbookercrawler.cfg"):
+    cfg_file = codecs.open(os.getcwd() + "\\..\\books\\hbookercrawler.cfg", 'r', 'utf-8')
+    cfg_file_line = cfg_file.readlines()
+    cfg_file.close()
+    for line in cfg_file_line:
+        if line.startswith("login_token="):
+            login_token = str_mid(line, 'login_token="', '"')
+        elif line.startswith("reader_id="):
+            reader_id = str_mid(line, 'reader_id="', '"')
+
+login_token = input('Cookie: login_token(默认为"' + login_token + '")=') or login_token
+reader_id = input('Cookie: reader_id(默认为"' + reader_id + '")=') or reader_id
+
+cfg_file = codecs.open(os.getcwd() + "\\..\\books\\hbookercrawler.cfg", 'w', 'utf-8')
+cfg_file.write('login_token="' + login_token + '"' + newline)
+cfg_file.write('reader_id="' + reader_id + '"' + newline)
+cfg_file.close()
 
 cj = http.cookiejar.CookieJar()
 
 cj.set_cookie(make_cookie("login_token", login_token))
 cj.set_cookie(make_cookie("reader_id", reader_id))
-cj.set_cookie(make_cookie("user_id", user_id))
+cj.set_cookie(make_cookie("user_id", reader_id))
 
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
@@ -101,8 +118,6 @@ def get_content(_chapter_id: str):
         if code == "100000":
             encryt_keys = str_mid(get_book_chapter_detail_info_str, '"encryt_keys":[', ']')
             chapter_content = str_mid(get_book_chapter_detail_info_str, '"chapter_content":"', '"')
-            encryt_keys = encryt_keys.replace('"', '').replace('\\', '')
-            chapter_content = chapter_content.replace('\\', '')
             _content = decrypt(chapter_content, encryt_keys, chapter_access_key)
             _content = re.sub(r"\r?\n", newline * 2, _content)
         else:
@@ -116,7 +131,7 @@ def get_content(_chapter_id: str):
 
 opener.addheaders = headers_default
 bookshelf_str = ''
-if login_token and reader_id and user_id:
+if login_token and reader_id:
     print("正在获取书架信息...")
     bookshelf_str = bytes(opener.open("http://www.hbooker.com/bookshelf/my_book_shelf/").read()).decode()
     nickname = str_mid(bookshelf_str, '<span class="J_Nickname">', '</span>')
@@ -141,7 +156,7 @@ if nickname or nickname == '${NoName}':
                 book_id = input("输入书籍序号或id(输入q退出):")
                 if not book_id:
                     continue
-                if book_id[0] == 'q':
+                if book_id.startswith('q'):
                     break
                 try:
                     if 0 < int(book_id) <= book_index:
@@ -154,7 +169,7 @@ if nickname or nickname == '${NoName}':
                 book_id = input("输入书籍id(输入q退出):")
                 if book_id:
                     break
-        if book_id[0] == 'q':
+        if book_id.startswith('q'):
             break
         print("正在获取书籍信息...")
         opener.addheaders = headers_default
@@ -169,16 +184,16 @@ if nickname or nickname == '${NoName}':
             book_chapter_index += 1
             book_chapter_info = (book_chapter_index,
                                  str_mid(str_, 'href="http://www.hbooker.com/chapter/book_chapter_detail/', '"'),
-                                 str_mid(str_, '</i>', '</a>'))
+                                 str_mid(str_, '</i>', '</a>').replace("<i class='icon-vip'></i>", ""))
             book_chapter.append(book_chapter_info)
         print("共", book_chapter_index, "章", "最新章节:", str_mid(book_chapter_str, '<div class="tit">', '</div>'))
         print("正在检查文件...")
-        if not os.path.isdir(os.getcwd() + "\\..\\books"):
-            os.mkdir(os.getcwd() + "\\..\\books")
         fixed_chapter = list()
         file_data = ''
         cnt_success = 0
         cnt_fail = 0
+        if not os.path.isdir(os.getcwd() + "\\..\\books"):
+            os.mkdir(os.getcwd() + "\\..\\books")
         if os.path.isfile(os.getcwd() + "\\..\\books\\" + book_title + ".txt"):
             file = codecs.open(os.getcwd() + "\\..\\books\\" + book_title + ".txt", 'r+', 'utf-8')
             file_data = file.read()
@@ -203,16 +218,37 @@ if nickname or nickname == '${NoName}':
         if len(fixed_chapter):
             print("章节修复完成，修复成功", cnt_success, "章，修复失败", cnt_fail, "章")
         print("输入新章节(留空将自动寻找)")
-        chapter_start = input("输入开始章节:") or 0
-        chapter_end = input("输入结束章节:") or book_chapter_index
-        if chapter_start == 0:
-            if file_data.rfind('No.') > -1:
-                chapter_start = str_mid(file_data, 'No.', '  ', file_data.rfind('No.'))
-            else:
-                chapter_start = 1
-                print("未能识别最新章节，将从第一章开始")
-        print("正在获取书籍内容...")
-        print(chapter_start, chapter_end)
+        while True:
+            chapter_start = input("输入开始章节:") or 0
+            chapter_end = input("输入结束章节:") or book_chapter_index
+            if chapter_start == 0:
+                if file_data.rfind('No.') > -1:
+                    try:
+                        chapter_start = int(str_mid(file_data, 'No.', '  ', file_data.rfind('No.'))) + 1
+                    except ValueError:
+                        chapter_start = 1
+                        print("未能识别最新章节，将从第一章开始")
+                else:
+                    chapter_start = 1
+                    print("未能识别最新章节，将从第一章开始")
+            try:
+                if int(chapter_start) <= int(chapter_end):
+                    print("开始章节:",
+                          "chapter_id:", book_chapter[int(chapter_start) - 1][1],
+                          "标题:", book_chapter[int(chapter_start) - 1][2])
+                    print("结束章节:",
+                          "chapter_id:", book_chapter[int(chapter_end) - 1][1],
+                          "标题:", book_chapter[int(chapter_end) - 1][2])
+                    while True:
+                        confirm = input("确定从这个位置下载吗(y/n):")
+                        if confirm.startswith('y') or confirm.startswith('n'):
+                            break
+                    if confirm.startswith('y'):
+                        break
+            except ValueError:
+                continue
+
+        print("正在下载书籍内容...")
         for chapter_index in range(int(chapter_start) - 1, int(chapter_end)):
             chapter_id = book_chapter[chapter_index][1]
             title = book_chapter[chapter_index][2]
