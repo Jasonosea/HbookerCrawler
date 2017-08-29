d899f3cf4cfeacf72bb889d40a6161f39b60c532
SeaLoong <984391132@qq.com>
2017-08-29 12:10:05 +0800

    支持图片章节的下载

--- HbookerCrawler.py
+++ HbookerCrawler.py
@@ -19,25 +19,29 @@ headers_default = [('Accept', 'text/html, application/xhtml+xml, application/xml
                    ('Accept-Language', 'zh-CN, zh; q=0.8'),
                    ('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'),
                    ('Host', 'www.hbooker.com')]
-headers_chapter_session_code = [('Accept', 'application/json, text/javascript, */*; q=0.01'),
-                                ('Accept-Encoding', 'deflate'),
-                                ('Accept-Language', 'zh-CN, zh; q=0.8'),
-                                ('Content-Length', '20'),
-                                ('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'),
-                                ('Host', 'www.hbooker.com'),
-                                ('Origin', 'http://www.hbooker.com'),
-                                ('Referer', 'http://www.hbooker.com/chapter/book_chapter_detail'),
-                                ('X-Requested-With', 'XMLHttpRequest')]
-headers_chapter_detail = [('Accept', 'application/json, text/javascript, */*; q=0.01'),
-                          ('Accept-Encoding', 'deflate'),
-                          ('Accept-Language', 'zh-CN, zh; q=0.8'),
-                          ('Content-Length', '48'),
-                          ('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'),
-                          ('Host', 'www.hbooker.com'),
-                          ('Origin', 'http://www.hbooker.com'),
-                          ('Referer', 'http://www.hbooker.com/chapter/book_chapter_detail'),
-                          ('X-Requested-With', 'XMLHttpRequest')]
+headers_chapter = [('Accept', 'application/json, text/javascript, */*; q=0.01'),
+                   ('Accept-Encoding', 'deflate'),
+                   ('Accept-Language', 'zh-CN, zh; q=0.8'),
+                   ('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'),
+                   ('Host', 'www.hbooker.com'),
+                   ('Origin', 'http://www.hbooker.com'),
+                   ('Referer', 'http://www.hbooker.com/chapter/book_chapter_detail'),
+                   ('X-Requested-With', 'XMLHttpRequest')]
+headers_book_chapter_image = [('Accept', 'image/webp,image/*,*/*;q=0.8'),
+                              ('Accept-Encoding', 'deflate'),
+                              ('Accept-Language', 'zh-CN, zh; q=0.8'),
+                              ('Host', 'www.hbooker.com'),
+                              ('Referer', 'http://www.hbooker.com/chapter/book_chapter_detail')]
 nl = '\r\n'
+doc_help = "下载的书籍文件及配置文件在 ../books 目录下。" + nl + \
+           "书籍文件有html和txt格式，使用html格式可加载图片，但确保图片文件存在。" + nl + \
+           "图片章节可通过修改配置文件中的area_width,font,font_size,bg_color_name,text_color_name实现不同文字效果" + nl + \
+           "area_width:图片宽度; 默认:816" + nl + \
+           "font:字体; 默认:undefined" + nl + \
+           "font_size:字体大小; 默认:14" + nl + \
+           "bg_color_name:背景颜色; 默认:default; 可用设置:default,green,blue,white,gray,pink,night;" + nl + \
+           "text_color_name:文字颜色; 默认:default; 可用设置:default,green,blue,white,gray,pink,night;" + nl + \
+           "图片章节保存在 ../books/<书名>/图片章节 目录下，书籍插图保存在 ../books/<书名>/插图 目录下"
 
 
 def make_cookie(name, value):
@@ -60,18 +64,21 @@ os.environ["NODE_PATH"] = os.getcwd() + "/node_modules"
 Decrypt_js = execjs.compile(open(os.getcwd() + '/Decrypt.js').read())
 
 
-def decrypt(_chapter_content: str, _encryt_keys: str, _chapter_access_key: str):
+def decrypt(_str: str, _encryt_keys: str, _access_key: str):
     _encryt_keys = _encryt_keys.replace('"', '').replace('\\', '')
-    _chapter_content = _chapter_content.replace('\\', '')
-    return str(Decrypt_js.call('decrypt', _chapter_content, _encryt_keys, _chapter_access_key))
+    _str = _str.replace('\\', '')
+    return str(Decrypt_js.call('decrypt', _str, _encryt_keys, _access_key))
 
-print("下载的书籍文件在 ../books 目录下。")
-print("书籍文件有html和txt格式，使用html格式可加载图片，但确保图片文件存在。")
 print("请先登录你的欢乐书客帐号，之后得到一些Cookies并输入程序。")
 print("若不登录则直接留空所有Cookies。")
 
 login_token = ""
 reader_id = ""
+area_width = "816"
+font = "undefined"
+font_size = "14"
+bg_color_name = "default"
+text_color_name = "default"
 if not os.path.isdir(os.getcwd() + "\\..\\books"):
     os.makedirs(os.getcwd() + "\\..\\books")
 if os.path.isfile(os.getcwd() + "\\..\\books\\hbookercrawler.cfg"):
@@ -81,15 +88,29 @@ if os.path.isfile(os.getcwd() + "\\..\\books\\hbookercrawler.cfg"):
             login_token = str_mid(line, 'login_token="', '"')
         elif line.startswith("reader_id="):
             reader_id = str_mid(line, 'reader_id="', '"')
+        elif line.startswith("area_width="):
+            area_width = str_mid(line, 'area_width="', '"')
+        elif line.startswith("font="):
+            font = str_mid(line, 'font="', '"')
+        elif line.startswith("font_size="):
+            font_size = str_mid(line, 'font_size="', '"')
+        elif line.startswith("bg_color_name="):
+            bg_color_name = str_mid(line, 'bg_color_name="', '"')
+        elif line.startswith("text_color_name="):
+            text_color_name = str_mid(line, 'text_color_name="', '"')
     cfg_file.close()
 
-login_token = input('Cookie: login_token(默认为"' + login_token + '")=') or login_token
-reader_id = input('Cookie: reader_id(默认为"' + reader_id + '")=') or reader_id
+login_token = input('Cookie: login_token(默认:"' + login_token + '")=') or login_token
+reader_id = input('Cookie: reader_id(默认:"' + reader_id + '")=') or reader_id
 
-cfg_file = codecs.open(os.getcwd() + "\\..\\books\\hbookercrawler.cfg", 'w', 'utf-8')
-cfg_file.write('login_token="' + login_token + '"' + nl)
-cfg_file.write('reader_id="' + reader_id + '"' + nl)
-cfg_file.close()
+with codecs.open(os.getcwd() + "\\..\\books\\hbookercrawler.cfg", 'w', 'utf-8') as cfg_file:
+    cfg_file.write('login_token="' + login_token + '"' + nl)
+    cfg_file.write('reader_id="' + reader_id + '"' + nl)
+    cfg_file.write('area_width="' + area_width + '"' + nl)
+    cfg_file.write('font="' + font + '"' + nl)
+    cfg_file.write('font_size="' + font_size + '"' + nl)
+    cfg_file.write('bg_color_name="' + bg_color_name + '"' + nl)
+    cfg_file.write('text_color_name="' + text_color_name + '"')
 del cfg_file
 
 cj = http.cookiejar.CookieJar()
@@ -99,19 +120,19 @@ cj.set_cookie(make_cookie("reader_id", reader_id))
 cj.set_cookie(make_cookie("user_id", reader_id))
 
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-opener_chapter_session_code = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-opener_chapter_detail = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+opener_chapter = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+opener_book_chapter_image = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
 opener.addheaders = headers_default
-opener_chapter_session_code.addheaders = headers_chapter_session_code
-opener_chapter_detail.addheaders = headers_chapter_detail
+opener_chapter.addheaders = headers_chapter
+opener_book_chapter_image.addheaders = headers_book_chapter_image
 
 
-def get_content(_chapter_id: str):
+def get_content(_chapter_id: str, _book_dir: str):
     _content = '<a href="http://www.hbooker.com/chapter/' + _chapter_id + '">章节链接</a>'
     try:
         post_data = str('chapter_id=' + _chapter_id).encode()
-        ajax_get_session_code_str = bytes(opener_chapter_session_code.open(
+        ajax_get_session_code_str = bytes(opener_chapter.open(
             "http://www.hbooker.com/chapter/ajax_get_session_code", post_data
         ).read()).decode('unicode_escape')
         code = str_mid(ajax_get_session_code_str, '"code":', ',')
@@ -119,7 +140,7 @@ def get_content(_chapter_id: str):
         if code == "100000":
             post_data = str('chapter_id=' + _chapter_id +
                             '&chapter_access_key=' + chapter_access_key).encode()
-            get_book_chapter_detail_info_str = bytes(opener_chapter_detail.open(
+            get_book_chapter_detail_info_str = bytes(opener_chapter.open(
                 "http://www.hbooker.com/chapter/get_book_chapter_detail_info", post_data
             ).read()).decode('unicode_escape')
             code = str_mid(get_book_chapter_detail_info_str, '"code":', ',')
@@ -128,6 +149,43 @@ def get_content(_chapter_id: str):
                 chapter_content = str_mid(get_book_chapter_detail_info_str, '"chapter_content":"', '"')
                 _content = decrypt(chapter_content, encryt_keys, chapter_access_key)
                 _content = re.sub("<p.+?>", '<p>', _content)
+            elif code == "400002":
+                try:
+                    # 图片章节/收费章节
+                    ajax_get_image_session_code_str = bytes(opener_chapter.open(
+                        "http://www.hbooker.com/chapter/ajax_get_image_session_code", b'\n'
+                    ).read()).decode('unicode_escape')
+                    code = str_mid(ajax_get_image_session_code_str, '"code":', ',')
+                    if code == "100000":
+                        encryt_keys = str_mid(ajax_get_image_session_code_str, '"encryt_keys":[', ']')
+                        access_key = str_mid(ajax_get_image_session_code_str, '"access_key":"', '"')
+                        image_code = str_mid(ajax_get_image_session_code_str, '"image_code":"', '"')
+                        image_code = decrypt(image_code, encryt_keys, access_key)
+                        opener_chapter.open("http://www.hbooker.com/chapter/get_book_chapter_image_height"
+                                            "?chapter_id=" + _chapter_id +
+                                            "&area_width=" + area_width +
+                                            "&font=" + font +
+                                            "&font_size=" + font_size +
+                                            "&image_code=" + image_code +
+                                            "&bg_color_name=" + bg_color_name +
+                                            "&text_color_name=" + text_color_name, b'\n')
+                        if not os.path.isdir(_book_dir + '\\图片章节'):
+                            os.makedirs(_book_dir + '\\图片章节')
+                        with open(_book_dir + '\\图片章节\\' + _chapter_id + '.png', 'wb') as _chapter_file:
+                            _chapter_file.write(opener_book_chapter_image.open(
+                                "http://www.hbooker.com/chapter/book_chapter_image"
+                                "?chapter_id=" + _chapter_id +
+                                "&area_width=" + area_width +
+                                "&font=" + font +
+                                "&font_size=" + font_size +
+                                "&image_code=" + image_code +
+                                "&bg_color_name=" + bg_color_name +
+                                "&text_color_name=" + text_color_name
+                            ).read())
+                        _content = '<img src="图片章节/' + _chapter_id + '.png" alt=\'chapter_id:' + _chapter_id + '\'>'
+                except Exception as _e:
+                    print("[ERROR]", _e)
+                    print("下载图片章节时出错")
             else:
                 tip = str_mid(get_book_chapter_detail_info_str, '"tip":"', '"')
                 print("[INFO]", "code:", code, "tip:", tip)
@@ -142,16 +200,17 @@ def get_content(_chapter_id: str):
 
 
 def download_image(_content: str, _book_dir: str):
-    if not os.path.isdir(_book_dir + '\\images'):
-        os.makedirs(_book_dir + '\\images')
-    for img in re.findall(r'<img src=.*?>', _content):
+    if not os.path.isdir(_book_dir + '\\插图'):
+        os.makedirs(_book_dir + '\\插图')
+    for img in re.findall(r'<img src="http.*?>', _content):
         try:
             src = str_mid(img, '<img src="', '"')
             if src.rfind('/') == -1:
                 continue
             filename = src[src.rfind('/') + 1:]
-            urllib.request.urlretrieve(src, _book_dir + '\\images\\' + filename)
-            _content = _content.replace(src, 'images/' + filename)
+            with open(_book_dir + '\\插图\\' + filename, 'wb') as _image:
+                _image.write(urllib.request.urlopen(src).read())
+            _content = _content.replace(src, '插图/' + filename)
         except Exception as _e:
             print("[ERROR]", _e)
             print("下载图片时出错")
@@ -163,12 +222,13 @@ def html2txt(_data: str):
         _data = _data.replace(_a, '章节链接:' + str_mid(_a, '<a href="', '"'))
     for _img in re.findall(r'<img src=.*?>', _data):
         _data = _data.replace(_img, '图片:"' + str_mid(_img, "alt='", "'") + '",' +
-                                    '图片位置:' + str_mid(_img, '<img src="', '"'))
+                                    '图片位置:"' + str_mid(_img, '<img src="', '"') + '"')
     _data = re.sub(r'<head>[\s\S]*?</head>', '', _data)
     _data = re.sub(r'</?(html|head|body|title|div|h|p).*?>', '', _data)
     _data = re.sub(r'[\r\n]+', nl * 2, _data)
     _data = _data.replace('<br>', '')
     return _data
+
 
 bookshelf_str = ''
 if login_token and reader_id:
@@ -204,11 +264,13 @@ if nickname:
             for book_info in bookshelf:
                 print("编号:", book_info[0], "id:", book_info[1], "书名:", book_info[2])
             while True:
-                book_id = input("输入书籍编号或id(输入q退出):").lower()
+                book_id = input("输入书籍编号或id(输入q:退出,h:帮助):").lower()
                 if not book_id:
                     continue
                 if book_id.startswith('q'):
                     break
+                if book_id.startswith('h'):
+                    print(doc_help)
                 try:
                     if 0 < int(book_id) <= book_index:
                         book_id = bookshelf[int(book_id) - 1][1]
@@ -217,8 +279,10 @@ if nickname:
                     continue
         else:
             while True:
-                book_id = input("输入书籍id(输入q退出):").lower()
-                if book_id:
+                book_id = input("输入书籍id(输入q:退出,h:帮助):").lower()
+                if book_id.startswith('h'):
+                    print(doc_help)
+                elif book_id:
                     break
         if book_id.startswith('q'):
             break
@@ -234,9 +298,8 @@ if nickname:
             for str_ in re.findall('<li><a target="_blank".*?</a>',
                                    book_chapter_str):
                 book_chapter_index += 1
-                book_chapter_info = (book_chapter_index,
-                                     str_mid(str_, 'href="http://www.hbooker.com/chapter/book_chapter_detail/', '"'),
-                                     str_mid(str_, '</i>', '</a>').replace("<i class='icon-vip'></i>", ""))
+                book_chapter_info = (str_mid(str_, 'href="http://www.hbooker.com/chapter/book_chapter_detail/', '"'),
+                                     str_mid(str_, '</i>', '</a>').replace("<i class='icon-vip'></i>", "[VIP]"))
                 book_chapter.append(book_chapter_info)
             print("共", book_chapter_index, "章", "最新章节:", str_mid(book_chapter_str, '<div class="tit">', '</div>'))
         except Exception as e:
@@ -264,7 +327,7 @@ if nickname:
                     if file_lines[i].startswith('<a href='):
                         chapter_id = str_mid(file_lines[i], '<a href="http://www.hbooker.com/chapter/', '">章节链接</a>')
                         print("尝试修复章节:", "chapter_id:", chapter_id, end="  ----  ")
-                        file_lines[i] = download_image(get_content(chapter_id), book_dir) + nl
+                        file_lines[i] = download_image(get_content(chapter_id, book_dir), book_dir) + nl
                         file.seek(0)
                         file.writelines(file_lines)
                         file.flush()
@@ -311,12 +374,12 @@ if nickname:
                     chapter_start = 1
                     print("未能识别最新章节编号，将从章节编号1开始")
                 if chapter_start <= chapter_end:
-                    print("开始章节编号:", book_chapter[chapter_start - 1][0],
-                          "chapter_id:", book_chapter[chapter_start - 1][1],
-                          "标题:", book_chapter[chapter_start - 1][2])
-                    print("结束章节编号:", book_chapter[chapter_end - 1][0],
-                          "chapter_id:", book_chapter[chapter_end - 1][1],
-                          "标题:", book_chapter[chapter_end - 1][2])
+                    print("开始章节编号:", chapter_start,
+                          "chapter_id:", book_chapter[chapter_start - 1][0],
+                          "标题:", book_chapter[chapter_start - 1][1])
+                    print("结束章节编号:", chapter_end,
+                          "chapter_id:", book_chapter[chapter_end - 1][0],
+                          "标题:", book_chapter[chapter_end - 1][1])
                     while True:
                         confirm = input("确定从这个位置下载吗(y/n/q):").lower()
                         if confirm.startswith('y') or confirm.startswith('n') or confirm.startswith('q'):
@@ -343,18 +406,24 @@ if nickname:
             cnt_success = 0
             cnt_fail = 0
             for chapter_index in range(chapter_start - 1, chapter_end):
-                chapter_id = book_chapter[chapter_index][1]
-                title = book_chapter[chapter_index][2]
-                print("章节编号:", book_chapter[chapter_index][0],
+                chapter_id = book_chapter[chapter_index][0]
+                title = book_chapter[chapter_index][1]
+                print("章节编号:", chapter_index + 1,
                       "chapter_id:", chapter_id,
                       "标题:", title, end="  ----  ")
-                content = download_image(get_content(chapter_id), book_dir)
+                content = download_image(get_content(chapter_id, book_dir), book_dir)
                 chapter_data = '<div id="' + str(chapter_index + 1) + '">' + nl + \
                                '<h3>' + title + '</h3>' + nl + \
                                content + nl + '<br><br>' + nl + \
                                '</div>' + nl
-                file_data += chapter_data
-                file.write(chapter_data)
+                if file_data.find('<div id="' + str(chapter_index + 1) + '">') == -1:
+                    file_data += chapter_data
+                    file.write(chapter_data)
+                else:
+                    file_data = re.sub('<div id="' + str(chapter_index + 1) + '">[\S\s]*?</div>[\r\n]*',
+                                       chapter_data, file_data)
+                    file.seek(0)
+                    file.write(file_data)
                 file.flush()
                 if content.startswith('<a href='):
                     cnt_fail += 1
